0dbd6b054909ea5414848ebb02fad3db91c2b3e9
Marcorzz575 <maruiz4607@gmail.com>
2026-05-15 15:38:45 -0600

    server

--- conexion.php
+++ conexion.php
@@ -1,13 +1,12 @@
 <?php
 // conexion.php
-$serverName = "TU_SERVIDOR";
+$serverName = "localhost";
 
-// Datos de conexión (Si usas Autenticación de Windows no necesitas usuario/contraseña)
+
 $connectionInfo = array(
     "Database" => "HotelDB",
     "CharacterSet" => "UTF-8"
-    // Si usas usuario "sa", descomenta la línea de abajo y pon tu contraseña:
-    // ,"UID" => "sa", "PWD" => "tu_contraseña"
+
 );
 
 $conn = sqlsrv_connect($serverName, $connectionInfo);
